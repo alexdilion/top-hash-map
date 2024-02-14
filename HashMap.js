03948bb9c2e6ad5e3704a1b415a65e065251ba85
alexdilion <alexdilion@protonmail.com>
2024-02-14 00:45:06 +0000

    Add hash method

--- HashMap.js
+++ HashMap.js
@@ -11,4 +11,14 @@ export default class HashMap {
             this.buckets.push(new LinkedList());
         }
     }
+
+    #hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
 }
