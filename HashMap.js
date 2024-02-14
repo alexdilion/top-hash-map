95cd415e220d9e215987ffc5780d18d0b4e36bd8
alexdilion <alexdilion@protonmail.com>
2024-02-14 00:15:27 +0000

    Add capacity property

--- HashMap.js
+++ HashMap.js
@@ -5,6 +5,7 @@ import LinkedList from "./LinkedList.js";
 export default class HashMap {
     constructor(capacity = 16) {
         this.buckets = [];
+        this.capacity = capacity;
 
         for (let i = 0; i < capacity; i++) {
             this.buckets.push(new LinkedList());
