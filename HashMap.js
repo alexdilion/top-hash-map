23cbeef6a8b91fd6bf680bc7bb79d44606793134
alexdilion <alexdilion@protonmail.com>
2024-02-15 11:51:02 +0000

    Add set method

--- HashMap.js
+++ HashMap.js
@@ -13,12 +13,20 @@ export default class HashMap {
     }
 
     #hash(key) {
-        let hashCode = 0;
         const primeNumber = 31;
+        let hashCode = 0;
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
 
         return hashCode;
+    }
+
+    // TODO: Expand map capacity when load factor is reached.
+    set(key, value) {
+        const keyHash = this.#hash(key);
+        const bucketIndex = keyHash % this.capacity;
+
+        this.buckets[bucketIndex].insert(key, value);
     }
 }
