c5e65c6013d285a4243d5c68457e32dbb9dc0258
alexdilion <alexdilion@protonmail.com>
2024-02-16 15:59:55 +0000

    Add dynamic resizing

--- HashMap.js
+++ HashMap.js
@@ -6,6 +6,7 @@ export default class HashMap {
     #buckets = [];
     #capacity = 16;
     #maxLoadFactor = 0.7;
+    #minLoadFactor = this.#maxLoadFactor / 4;
     #length = 0;
 
     constructor(capacity = 16) {
@@ -37,6 +38,40 @@ export default class HashMap {
         return this.#hash(key) % this.#capacity;
     }
 
+    rehashEntries() {
+        const newMap = [];
+        const entries = this.entries;
+
+        for (let i = 0; i < this.#capacity; i++) {
+            newMap.push(new LinkedList());
+        }
+
+        this.#buckets = newMap;
+
+        entries.forEach((entry) => {
+            const [key, value] = entry;
+            const bucketIndex = this.#getBucketIndex(key);
+            this.#buckets[bucketIndex].insert(key, value);
+        });
+    }
+
+    optimiseSize() {
+        const loadFactor = this.#length / this.#capacity;
+
+        if (loadFactor >= this.#maxLoadFactor) {
+            this.#capacity *= 2;
+
+            this.rehashEntries();
+        } else if (loadFactor < this.#minLoadFactor && this.#capacity > 16) {
+            // this is to account for when we clear the map
+            while (loadFactor < this.#minLoadFactor && this.#capacity > 16) {
+                this.#capacity /= 2;
+            }
+
+            this.rehashEntries();
+        }
+    }
+
     // TODO: Expand map capacity when load factor is reached.
     set(key, value) {
         const bucketIndex = this.#getBucketIndex(key);
@@ -44,6 +79,7 @@ export default class HashMap {
 
         if (!keyExists) {
             this.#length += 1;
+            this.optimiseSize();
         }
     }
 
@@ -67,14 +103,17 @@ export default class HashMap {
 
         if (entryRemoved) {
             this.#length -= 1;
+            this.optimiseSize();
         }
 
         return entryRemoved;
     }
 
     clear() {
+        this.#buckets = this.#buckets.slice(0, 16);
         this.#buckets.forEach((bucket) => bucket.clear());
         this.#length = 0;
+        this.#capacity = 16;
     }
 
     get length() {
@@ -104,5 +143,9 @@ export default class HashMap {
         this.#buckets.map((bucket) => entries.push(...bucket.entries));
 
         return entries;
+    }
+
+    get capacity() {
+        return this.#capacity;
     }
 }
